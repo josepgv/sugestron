6aa16fc8e09cedc135a9c4dddbf108ae1ea03fdf
josepgv <jgarrigues@gmail.com>
2014-05-01 11:35:24 +0200

    Show topics

--- public/index.php
+++ public/index.php
@@ -12,3 +12,10 @@ $data = new TopicData();
 $data->connect();
 
 $topics = $data->getAllTopics();
+
+foreach($topics as $topic) {
+    echo "<h3>" . $topic["title"] . " (" . $topic["id"] . ")</h3>";
+    echo "<p>";
+    echo nl2br($topic["description"]);
+    echo "</p>";
+}
