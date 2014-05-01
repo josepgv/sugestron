d2fac230e1787d04aa12df9cd26149963bdc2065
josepgv <jgarrigues@gmail.com>
2014-05-01 11:39:21 +0200

    Adding form

---
+++ public/add.php
@@ -0,0 +1,14 @@
+<h2>Add a topic</h2>
+
+<form action="add.php" method="POST">
+    <label>
+        Title: <input type="text" name="title">
+    </label>
+    <br>
+    <label>
+        Description <br>
+        <textarea name="description" cols="50" rows="50"></textarea>
+    </label>
+    <br>
+    <input type="submit" value="Add!">
+</form>
